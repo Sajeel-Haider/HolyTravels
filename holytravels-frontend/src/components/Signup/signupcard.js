da8a20fe278c4812f4658c389b1c6d0830a76b2b
LabridTech <146828571+LabridTech@users.noreply.github.com>
2024-02-24 18:30:31 +0500

    signup complete

--- holytravels-frontend/src/components/Signup/signupcard.js
+++ holytravels-frontend/src/components/Signup/signupcard.js
@@ -11,19 +11,23 @@ import { Link } from '@mui/material';
 
 export default function SigninCard() {
   return (
-    <Box sx={{ flexDirection : 'column'  , Width : 10 , display : 'flex' , gap : 2 }} >
+    <Box sx={{ flexDirection : 'column'  , display : 'flex' , gap : 2 }} width={400} height={615}>
     <Card sx={{ border : 1 , borderColor : '#b49164' , padding : "10%" ,  }}>
-      <CardContent sx={{flexDirection : 'column' , display : 'flex' , justifyContent : 'center' , alignItems : 'center', gap : 2 , marginBottom : 3 }}>
+      <CardContent sx={{flexDirection : 'column' , display : 'flex' , justifyContent : 'center' , alignItems : 'center', gap : 2 , }}>
       <Typography gutterBottom variant="h5" component="div" sx={{color: '#b49164'}}>
           Holy Travels
         </Typography>
-        <TextField variant='outlined' label="UserName" ></TextField>
+        <TextField variant='outlined' label="UserName" 
+        ></TextField>
+        <TextField variant='outlined' label="Email" ></TextField>
+        <TextField variant='outlined' label="City" ></TextField>
+        <TextField variant='outlined' label="address" ></TextField>
+        <TextField variant='outlined' label="Phone " ></TextField>
         <TextField variant='outlined' label="Password" ></TextField>
-        <TextField variant='outlined' label="UserName" ></TextField>
-        <TextField variant='outlined' label="Password" ></TextField>
+       
       </CardContent>
       <CardActions sx={{  marginBottom : 2}} >
-        <Button variant="contained" size='large' sx={{backgroundColor : '#b49164' , minWidth : 1 }} >Sign in </Button>
+      <Button variant="contained" size='large' sx={{backgroundColor : '#b49164' , minWidth : 1 }} >Sign in </Button>
       </CardActions>
     </Card>
     </Box>
