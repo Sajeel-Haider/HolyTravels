2d84e905f7fff1dff47e11a60301eefe1cc7d9b5
LabridTech <146828571+LabridTech@users.noreply.github.com>
2024-02-24 17:45:19 +0500

    Signup add

--- holytravels-frontend/src/App.js
+++ holytravels-frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Main } from "./pages/Main/Main";
 import { Routes, Route } from "react-router";
 import Login from "./pages/Login/Login"
+import Signin from "./pages/Signup/Signup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Signup" element={<Signin />} />
         <Route path="*" element={<>Page not found</>} />
       </Routes>
     </>
