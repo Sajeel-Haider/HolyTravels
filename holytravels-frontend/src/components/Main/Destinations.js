bd7d0a87e7aa135954b8e831c89eabef85980a0b
LabridTech <146828571+LabridTech@users.noreply.github.com>
2024-02-25 03:32:36 +0500

    ahmad to main page

--- holytravels-frontend/src/components/Main/Destinations.js
+++ holytravels-frontend/src/components/Main/Destinations.js
@@ -3,6 +3,8 @@ import { Stack } from "@mui/system";
 import ExploreTourCard from "../../utils/Cards/Main/ExploreTourCard";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import React, { useState } from 'react';
 
 import badshahiMosque from "../../assets/badshahiMosque.jpg";
 import faisal from "../../assets/faisal.jpg";
@@ -13,58 +15,61 @@ import fairy from"../../assets/fairy_medows.jpg"
 import mohenjo from "../../assets/mohenjo_daro.jpg"
 import wazir from "../../assets/wazir_khan.jpg"
 import trango from "../../assets/trango_towers.jpg"
+
 export const Destinations = () => {
+  const beigeColor = "#F5F5DC"; // Beige color code
+
   const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    backgroundColor: beigeColor, // Set background color to beige
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: "center",
     color: theme.palette.text.secondary,
   }));
 
+  const [numAdventurousPlaces, setNumAdventurousPlaces] = useState(3);
+  const [numCulturalPlaces, setNumCulturalPlaces] = useState(3);
+  const [numNaturalWonders, setNumNaturalWonders] = useState(3);
+
+  const adventurousPlaces = [
+    { img: trango, name: "Trango Towers" },
+    { img: anso, name: "Derawar Fort" },
+    { img: fairy, name: "Fairy Meadows" },
+  ];
+
+  const culturalPlaces = [
+    { img: badshahiMosque, name: "Badshahi Mosque" },
+    { img: wazir, name: "Wazir Khan" },
+
+    { img: faisal, name: "Faisal Mosque" },
+    { img: lahorefort, name: "Lahore Fort" },
+    { img: mohenjo, name: "Mohenjo Daro" },
+    { img: anso, name: "Derawar Fort" },
+  ];
+
+  const naturalWonders = [
+    { img: deosai, name: "Deosai Plains" },
+    { img: trango, name: "Trango Towers" },
+    { img: fairy, name: "Fairy Meadows" },
+  ];
+
+  const handleViewMoreAdventurousPlaces = () => {
+    setNumAdventurousPlaces(adventurousPlaces.length);
+  };
+
+  const handleViewMoreCulturalPlaces = () => {
+    setNumCulturalPlaces(culturalPlaces.length);
+  };
+
+  const handleViewMoreNaturalWonders = () => {
+    setNumNaturalWonders(naturalWonders.length);
+  };
+
   return (
-    <Stack direction={"column"} justifyContent={"center"} padding={"5%"}>
+    <Stack direction={"column"} justifyContent={"center"} padding={"5%"} style={{ backgroundColor: beigeColor }}>
       <Stack alignItems={"center"}>
         <Typography variant="h4" fontWeight={600}>
           Explore Natural Wonders
-        </Typography>
-      </Stack>
-      `<Stack marginTop={"50px"}>
-        <Grid
-          container
-          justifyContent={"center"}
-          rowSpacing={1}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        >
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={trango}
-              name={"=Tranhgo"}
-            ></ExploreTourCard>
-          </Grid>
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={anso}
-              name={"=Tranhgo"}
-            ></ExploreTourCard>
-          </Grid>
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={fairy}
-              name={"=Tranhgo"}
-            ></ExploreTourCard>
-          </Grid>
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={faisal}
-              name={"Faisal Mosque"}
-            ></ExploreTourCard>
-          </Grid>
-        </Grid>
-      </Stack>
-      <Stack>
-        <Typography variant="h4" fontWeight={600} alignItems={"center"}>
-          Explore Historical Places
         </Typography>
       </Stack>
       <Stack marginTop={"50px"}>
@@ -74,31 +79,48 @@ export const Destinations = () => {
           rowSpacing={1}
           columnSpacing={{ xs: 1, sm: 2, md: 3 }}
         >
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={badshahiMosque}
-              name={"Badshahi Mosque"}
-            ></ExploreTourCard>
-          </Grid>
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={wazir}
-              name={"Faisal Mosque"}
-            ></ExploreTourCard>
-          </Grid>
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={faisal}
-              name={"Faisal Mosque"}
-            ></ExploreTourCard>
-          </Grid>
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={mohenjo}
-              name={"Faisal Mosque"}
-            ></ExploreTourCard>
-          </Grid>
+          {naturalWonders.slice(0, numNaturalWonders).map((place) => (
+            <Grid item xs={4} key={place.name}>
+              <ExploreTourCard img1={place.img} name={place.name} />
+            </Grid>
+          ))}
         </Grid>
+        {numNaturalWonders < naturalWonders.length && (
+          <Stack alignItems="center">
+            <Button variant="contained" onClick={handleViewMoreNaturalWonders}>
+              View More
+            </Button>
+          </Stack>
+        )}
+      </Stack>
+      <Stack>
+        <Typography variant="h4" fontWeight={600} alignItems={"center"}>
+          Explore Cultural Places
+        </Typography>
+      </Stack>
+
+
+      
+      <Stack marginTop={"50px"}>
+        <Grid
+          container
+          justifyContent={"center"}
+          rowSpacing={1}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        >
+          {culturalPlaces.slice(0, numCulturalPlaces).map((place) => (
+            <Grid item xs={4} key={place.name}>
+              <ExploreTourCard img1={place.img} name={place.name}  style={{ width: '500px', height: '250px' }} />
+            </Grid>
+          ))}
+        </Grid>
+        {numCulturalPlaces < culturalPlaces.length && (
+          <Stack alignItems="center">
+            <Button variant="contained" onClick={handleViewMoreCulturalPlaces}>
+              View More
+            </Button>
+          </Stack>
+        )}
       </Stack>
       <Stack>
         <Typography variant="h4" fontWeight={600} alignItems={"center"}>
@@ -112,52 +134,19 @@ export const Destinations = () => {
           rowSpacing={1}
           columnSpacing={{ xs: 1, sm: 2, md: 3 }}
         >
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={badshahiMosque}
-              name={"Badshahi Mosque"}
-            ></ExploreTourCard>
-          </Grid>
-
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={faisal}
-              name={"Faisal Mosque"}
-            ></ExploreTourCard>
-          </Grid>
+          {adventurousPlaces.slice(0, numAdventurousPlaces).map((place) => (
+            <Grid item xs={4} key={place.name}>
+              <ExploreTourCard img1={place.img} name={place.name} />
+            </Grid>
+          ))}
         </Grid>
-      </Stack>
-      <Stack>
-        <Typography variant="h4" fontWeight={600} alignItems={"center"}>
-          Explore Cultural Places
-        </Typography>
-      </Stack>
-      <Stack marginTop={"50px"}>
-        <Grid
-          container
-          justifyContent={"center"}
-          rowSpacing={1}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        >
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={badshahiMosque}
-              name={"Badshahi Mosque"}
-            ></ExploreTourCard>
-          </Grid>
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={mohenjo}
-              name={"Lahore Fort"}
-            ></ExploreTourCard>
-          </Grid>
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={faisal}
-              name={"Faisal Mosque"}
-            ></ExploreTourCard>
-          </Grid>
-        </Grid>
+        {numAdventurousPlaces < adventurousPlaces.length && (
+          <Stack alignItems="center">
+            <Button variant="contained" onClick={handleViewMoreAdventurousPlaces}>
+              View More
+            </Button>
+          </Stack>
+        )}
       </Stack>
     </Stack>
   );
