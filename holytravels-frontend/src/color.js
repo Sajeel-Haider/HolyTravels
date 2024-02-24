f6fe5dbd09c154b5580e153315de05afca367678
Sajeel-Haider <sajeelshp1@gmail.com>
2024-02-24 17:03:24 +0500

    * added responsivenesss theme

--- holytravels-frontend/src/color.js
+++ holytravels-frontend/src/color.js
@@ -1,18 +1,27 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
   palette: {
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 900,
+        lg: 1200,
+        xl: 1536,
+      },
+    },
     primary: {
-      main: '#b49164',
+      main: "#b49164",
       // light: will be calculated from palette.primary.main,
       // dark: will be calculated from palette.primary.main,
       // contrastText: will be calculated to contrast with palette.primary.main
     },
     secondary: {
-      main: '#E0C2FF',
-      light: '#F5EBFF',
+      main: "#E0C2FF",
+      light: "#F5EBFF",
       // dark: will be calculated from palette.secondary.main,
-      contrastText: '#47008F',
+      contrastText: "#47008F",
     },
   },
 });
--- holytravels-frontend/src/components/Main/Header.js
+++ holytravels-frontend/src/components/Main/Header.js
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material";
 import { Stack } from "@mui/system";
-
+import lamp from "../../assets/lamp.jpg";
+import { round } from "@floating-ui/utils";
 export const Header = () => {
   return (
-    <Stack direction={"row"}>
-      <Stack width={"50%"} padding={"10%"}>
+    <Stack direction={{ xs: "column", md: "row" }}>
+      <Stack width={{ md: "50%", xs: "100%" }} padding={"10%"}>
         <Stack spacing={3}>
           <Typography variant="h3">Holy Travels</Typography>
           <Typography variant="body1">
@@ -12,8 +13,8 @@ export const Header = () => {
           </Typography>
         </Stack>
       </Stack>
-      <Stack width={"50%"} padding={"10%"}>
-        <img src="" alt="" />
+      <Stack width={{ md: "50%", xs: "100%" }} border={round} padding={"5%"}>
+        <img src={lamp} alt="" style={{ border: "20px" }} />
       </Stack>
     </Stack>
   );
