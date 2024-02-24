cbba76d2a631aae9ffe58a9e3ab8fea6dfc4ea76
LabridTech <146828571+LabridTech@users.noreply.github.com>
2024-02-25 02:24:45 +0500

    ahmad change

--- holytravels-frontend/src/components/Main/Destinations.js
+++ holytravels-frontend/src/components/Main/Destinations.js
@@ -6,6 +6,13 @@ import Paper from "@mui/material/Paper";
 
 import badshahiMosque from "../../assets/badshahiMosque.jpg";
 import faisal from "../../assets/faisal.jpg";
+import lahorefort from "../../assets/lahore_fort.jpg"
+import deosai from "../../assets/deosai.jpg"
+import anso from "../../assets/derawar_fort.jpg"
+import fairy from"../../assets/fairy_medows.jpg"
+import mohenjo from "../../assets/mohenjo_daro.jpg"
+import wazir from "../../assets/wazir_khan.jpg"
+import trango from "../../assets/trango_towers.jpg"
 export const Destinations = () => {
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -19,6 +26,44 @@ export const Destinations = () => {
     <Stack direction={"column"} justifyContent={"center"} padding={"5%"}>
       <Stack alignItems={"center"}>
         <Typography variant="h4" fontWeight={600}>
+          Explore Natural Wonders
+        </Typography>
+      </Stack>
+      `<Stack marginTop={"50px"}>
+        <Grid
+          container
+          justifyContent={"center"}
+          rowSpacing={1}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        >
+          <Grid item xs={6}>
+            <ExploreTourCard
+              img1={trango}
+              name={"=Tranhgo"}
+            ></ExploreTourCard>
+          </Grid>
+          <Grid item xs={6}>
+            <ExploreTourCard
+              img1={anso}
+              name={"=Tranhgo"}
+            ></ExploreTourCard>
+          </Grid>
+          <Grid item xs={6}>
+            <ExploreTourCard
+              img1={fairy}
+              name={"=Tranhgo"}
+            ></ExploreTourCard>
+          </Grid>
+          <Grid item xs={6}>
+            <ExploreTourCard
+              img1={faisal}
+              name={"Faisal Mosque"}
+            ></ExploreTourCard>
+          </Grid>
+        </Grid>
+      </Stack>
+      <Stack>
+        <Typography variant="h4" fontWeight={600} alignItems={"center"}>
           Explore Historical Places
         </Typography>
       </Stack>
@@ -37,33 +82,19 @@ export const Destinations = () => {
           </Grid>
           <Grid item xs={6}>
             <ExploreTourCard
+              img1={wazir}
+              name={"Faisal Mosque"}
+            ></ExploreTourCard>
+          </Grid>
+          <Grid item xs={6}>
+            <ExploreTourCard
               img1={faisal}
               name={"Faisal Mosque"}
             ></ExploreTourCard>
           </Grid>
-        </Grid>
-      </Stack>
-      <Stack>
-        <Typography variant="h4" fontWeight={600} alignItems={"center"}>
-          Explore Religious Places
-        </Typography>
-      </Stack>
-      <Stack marginTop={"50px"}>
-        <Grid
-          container
-          justifyContent={"center"}
-          rowSpacing={1}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        >
           <Grid item xs={6}>
             <ExploreTourCard
-              img1={badshahiMosque}
-              name={"Badshahi Mosque"}
-            ></ExploreTourCard>
-          </Grid>
-          <Grid item xs={6}>
-            <ExploreTourCard
-              img1={faisal}
+              img1={mohenjo}
               name={"Faisal Mosque"}
             ></ExploreTourCard>
           </Grid>
@@ -87,6 +118,7 @@ export const Destinations = () => {
               name={"Badshahi Mosque"}
             ></ExploreTourCard>
           </Grid>
+
           <Grid item xs={6}>
             <ExploreTourCard
               img1={faisal}
@@ -111,6 +143,12 @@ export const Destinations = () => {
             <ExploreTourCard
               img1={badshahiMosque}
               name={"Badshahi Mosque"}
+            ></ExploreTourCard>
+          </Grid>
+          <Grid item xs={6}>
+            <ExploreTourCard
+              img1={mohenjo}
+              name={"Lahore Fort"}
             ></ExploreTourCard>
           </Grid>
           <Grid item xs={6}>
